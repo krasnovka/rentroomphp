950e2a789141c4dbc4f89255d6b7c8c335f6c92d
krasnovka <111653479+krasnovka@users.noreply.github.com>
2026-04-08 10:43:30 +0300

    change comments

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ $db_name = 'varo_db';
 $db_user = 'root';
 $db_pass = '';
 
-// Change DB settings here for your XAMPP MySQL
+// Change DB settings here 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
